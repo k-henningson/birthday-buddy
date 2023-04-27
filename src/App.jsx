cf0d92ced891c7e467bf49a3f4b36fdf2825311d
Kyra Henningson <kyrahenningson@hotmail.com>
2023-04-27 16:05:44 -0700

    update title, change button padding, add emojis to title

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,15 @@ const App = () => {
   return (
     <main>
       <section className="container">
-        <h3>{people.length} birthdays today</h3>
+        <h3>{people.length} birthdays today 🎂 🎈</h3>
         <List people={people} />
+        <button
+          type="button"
+          className="btn btn-block"
+          onClick={() => setPeople([])}
+        >
+          clear all
+        </button>
       </section>
     </main>
   );
